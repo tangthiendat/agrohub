b1f0dd668546b4ccb791b1b44a10f470ff5b52da
Tang Thien Dat <thiendattang@gmail.com>
2025-01-06 19:50:48 +0700

    refactor: remove aggregate

--- auth-service/src/main/java/com/ttdat/authservice/application/commands/CreateUserCommand.java
+++ auth-service/src/main/java/com/ttdat/authservice/application/commands/CreateUserCommand.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
-import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 import java.util.UUID;
 
@@ -14,7 +13,6 @@ import java.util.UUID;
 @AllArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class CreateUserCommand {
-    @TargetAggregateIdentifier
     UUID userId;
     String fullName;
     Gender gender;
