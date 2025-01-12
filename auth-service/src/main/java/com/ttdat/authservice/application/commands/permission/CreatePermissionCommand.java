4cefb689cebf4efd81a9c865ad32a245296c5a20
Tang Thien Dat <thiendattang@gmail.com>
2025-01-12 08:02:03 +0700

    refactor: add json creator for commands

--- auth-service/src/main/java/com/ttdat/authservice/application/commands/permission/CreatePermissionCommand.java
+++ auth-service/src/main/java/com/ttdat/authservice/application/commands/permission/CreatePermissionCommand.java
@@ -1,13 +1,13 @@
 package com.ttdat.authservice.application.commands.permission;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Builder
-@AllArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class CreatePermissionCommand {
     String permissionName;
@@ -15,4 +15,17 @@ public class CreatePermissionCommand {
     String apiPath;
     String httpMethod;
     String module;
+
+    @JsonCreator
+    public CreatePermissionCommand(@JsonProperty("permissionName") String permissionName,
+                                   @JsonProperty("description") String description,
+                                   @JsonProperty("apiPath") String apiPath,
+                                   @JsonProperty("httpMethod") String httpMethod,
+                                   @JsonProperty("module") String module) {
+        this.permissionName = permissionName;
+        this.description = description;
+        this.apiPath = apiPath;
+        this.httpMethod = httpMethod;
+        this.module = module;
+    }
 }
--- auth-service/src/main/java/com/ttdat/authservice/application/commands/permission/UpdatePermissionCommand.java
+++ auth-service/src/main/java/com/ttdat/authservice/application/commands/permission/UpdatePermissionCommand.java
@@ -1,13 +1,13 @@
 package com.ttdat.authservice.application.commands.permission;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Builder
-@AllArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class UpdatePermissionCommand {
     Long permissionId;
@@ -16,4 +16,19 @@ public class UpdatePermissionCommand {
     String apiPath;
     String httpMethod;
     String module;
+
+    @JsonCreator
+    public UpdatePermissionCommand(@JsonProperty("permissionId") Long permissionId,
+                                   @JsonProperty("permissionName") String permissionName,
+                                   @JsonProperty("description") String description,
+                                   @JsonProperty("apiPath") String apiPath,
+                                   @JsonProperty("httpMethod") String httpMethod,
+                                   @JsonProperty("module") String module) {
+        this.permissionId = permissionId;
+        this.permissionName = permissionName;
+        this.description = description;
+        this.apiPath = apiPath;
+        this.httpMethod = httpMethod;
+        this.module = module;
+    }
 }
