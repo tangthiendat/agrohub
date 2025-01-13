d3211650dad54718a8911afeb221eb80314a2057
Tang Thien Dat <thiendattang@gmail.com>
2025-01-13 14:38:34 +0700

    refactor: add json creator for missing query

--- auth-service/src/main/java/com/ttdat/authservice/application/queries/user/GetUserByEmailQuery.java
+++ auth-service/src/main/java/com/ttdat/authservice/application/queries/user/GetUserByEmailQuery.java
@@ -1,14 +1,19 @@
 package com.ttdat.authservice.application.queries.user;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Builder
-@AllArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class GetUserByEmailQuery {
     String email;
+
+    @JsonCreator
+    public GetUserByEmailQuery(@JsonProperty("email") String email) {
+        this.email = email;
+    }
 }
