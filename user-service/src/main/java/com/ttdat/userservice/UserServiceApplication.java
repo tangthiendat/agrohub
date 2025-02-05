192f57f564e2970c5d8a028752e69213e80ad4eb
Tang Thien Dat <thiendattang@gmail.com>
2025-02-05 13:24:37 +0700

    refactor: remove unused code

--- user-service/src/main/java/com/ttdat/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/ttdat/userservice/UserServiceApplication.java
@@ -3,12 +3,10 @@ package com.ttdat.userservice;
 import com.ttdat.userservice.infrastructure.config.context.DotEnvApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@ComponentScan(basePackages = {"com.ttdat.userservice", "com.ttdat.core"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
