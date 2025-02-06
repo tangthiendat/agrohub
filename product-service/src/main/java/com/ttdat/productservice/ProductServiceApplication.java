71ceadad451677e87f775c48666e9f7f549def8b
Tang Thien Dat <thiendattang@gmail.com>
2025-02-06 13:42:02 +0700

    refactor: remove env context initializer

--- product-service/src/main/java/com/ttdat/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ttdat/productservice/ProductServiceApplication.java
@@ -1,16 +1,15 @@
 package com.ttdat.productservice;
 
-import com.ttdat.productservice.infrastructure.config.context.DotEnvApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(ProductServiceApplication.class);
-        springApplication.addInitializers(new DotEnvApplicationContextInitializer(".env.dev"));
-        springApplication.run(args);
+        SpringApplication.run(ProductServiceApplication.class, args);
     }
 
 }
--- user-service/src/main/java/com/ttdat/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/ttdat/userservice/UserServiceApplication.java
@@ -1,6 +1,5 @@
 package com.ttdat.userservice;
 
-import com.ttdat.userservice.infrastructure.config.context.DotEnvApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,9 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(UserServiceApplication.class);
-        springApplication.addInitializers(new DotEnvApplicationContextInitializer(".env.dev"));
-        springApplication.run(args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 
