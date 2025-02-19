adfde2ce2f448da3f48d9c7ad8a991caf51e9b22
Tang Thien Dat <thiendattang@gmail.com>
2025-02-19 19:59:24 +0700

    feat: add supplier specification

--- purchase-service/src/main/java/com/ttdat/purchaseservice/application/handlers/query/SupplierQueryHandler.java
+++ purchase-service/src/main/java/com/ttdat/purchaseservice/application/handlers/query/SupplierQueryHandler.java
@@ -6,11 +6,15 @@ import com.ttdat.purchaseservice.application.queries.supplier.GetSupplierPageQue
 import com.ttdat.purchaseservice.domain.entities.Supplier;
 import com.ttdat.purchaseservice.domain.repositories.SupplierRepository;
 import com.ttdat.purchaseservice.infrastructure.utils.PaginationUtils;
+import com.ttdat.purchaseservice.infrastructure.utils.SpecificationUtils;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -21,11 +25,31 @@ public class SupplierQueryHandler {
     @QueryHandler
     public SupplierPageResult handle(GetSupplierPageQuery query) {
         Pageable pageable = PaginationUtils.getPageable(query.getPaginationParams(), query.getSortParams());
-        Page<Supplier> supplierPage = supplierRepository.findAll(pageable);
+        Specification<Supplier> supplierPageSpec = getSupplierPageSpec(query.getFilterParams());
+        Page<Supplier> supplierPage = supplierRepository.findAll(supplierPageSpec, pageable);
         return SupplierPageResult.builder()
                 .meta(PaginationUtils.getPaginationMeta(supplierPage))
                 .content(supplierMapper.toDTOList(supplierPage.getContent()))
                 .build();
+    }
+
+    private Specification<Supplier> getSupplierPageSpec(Map<String, String> filterParams){
+        Specification<Supplier> supplierPageSpec = Specification.where(null);
+        supplierPageSpec = supplierPageSpec.and(SpecificationUtils.buildSpecification(filterParams, "active", Boolean.class));
+        if (filterParams.containsKey("query")){
+            String searchValue = filterParams.get("query").toLowerCase();
+            Specification<Supplier> querySpec = (root, query, criteriaBuilder) -> {
+                String likePattern = "%" + searchValue + "%";
+                return criteriaBuilder.or(
+                        criteriaBuilder.like(root.get("supplierId"), likePattern),
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("supplierName")), likePattern),
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), likePattern),
+                        criteriaBuilder.like(criteriaBuilder.lower(root.get("phoneNumber")), likePattern)
+                );
+            };
+            supplierPageSpec = supplierPageSpec.and(querySpec);
+        }
+        return supplierPageSpec;
     }
 
 }
