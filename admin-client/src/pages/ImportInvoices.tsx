1e68d5543ca6649eb35ce168dd50abc6705c08bc
Tang Thien Dat <thiendattang@gmail.com>
2025-03-20 19:57:47 +0700

    feat: search import invoice

--- admin-client/src/pages/ImportInvoices.tsx
+++ admin-client/src/pages/ImportInvoices.tsx
@@ -1,7 +1,11 @@
 import { Button } from "antd";
+import { SearchProps } from "antd/es/input";
+import Search from "antd/es/input/Search";
 import { PlusOutlined } from "@ant-design/icons";
 import { useNavigate, useSearchParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
 import Access from "../features/auth/Access";
+import ImportInvoiceTable from "../features/import-invoice/ImportInvoiceTable";
 import { useTitle } from "../common/hooks";
 import { PERMISSIONS } from "../common/constants";
 import { Module } from "../common/enums";
@@ -11,8 +15,6 @@ import {
   SortParams,
 } from "../interfaces";
 import { importInvoiceService } from "../services";
-import { useQuery } from "@tanstack/react-query";
-import ImportInvoiceTable from "../features/import-invoice/ImportInvoiceTable";
 
 const ImportInvoices: React.FC = () => {
   useTitle("Phiếu nhập kho");
@@ -30,12 +32,12 @@ const ImportInvoices: React.FC = () => {
     direction: searchParams.get("direction") || "",
   };
 
-  // const filter: ImportInvoiceFilterCriteria = {
-  //   query: searchParams.get("query") || undefined,
-  // };
+  const filter: ImportInvoiceFilterCriteria = {
+    query: searchParams.get("query") || undefined,
+  };
 
   const { data, isLoading } = useQuery({
-    queryKey: ["import-invoices", pagination, sort].filter((key) => {
+    queryKey: ["import-invoices", pagination, sort, filter].filter((key) => {
       if (typeof key === "string") {
         return key !== "";
       } else if (key instanceof Object) {
@@ -44,14 +46,34 @@ const ImportInvoices: React.FC = () => {
         );
       }
     }),
-    queryFn: () => importInvoiceService.getPage(pagination, sort),
+    queryFn: () => importInvoiceService.getPage(pagination, sort, filter),
   });
+
+  const handleSearch: SearchProps["onSearch"] = (value) => {
+    if (value) {
+      searchParams.set("query", value);
+    } else {
+      searchParams.delete("query");
+    }
+    setSearchParams(searchParams);
+  };
 
   return (
     <Access permission={PERMISSIONS[Module.IMPORT_INVOICE].GET_PAGE}>
       <div className="card">
-        <div className="mb-2 flex items-center justify-between">
-          <h2 className="text-xl font-semibold">Phiếu nhập kho</h2>
+        <h2 className="mb-3 text-xl font-semibold">Phiếu nhập kho</h2>
+        <div className="mb-5 flex items-center justify-between">
+          <div className="w-[40%]">
+            <div className="flex justify-between gap-3">
+              <Search
+                placeholder="Tìm kiếm theo mã phiếu nhập hoặc tên nhà cung cấp"
+                defaultValue={searchParams.get("query") || ""}
+                enterButton
+                allowClear
+                onSearch={handleSearch}
+              />
+            </div>
+          </div>
           <Access
             permission={PERMISSIONS[Module.IMPORT_INVOICE].CREATE}
             hideChildren
