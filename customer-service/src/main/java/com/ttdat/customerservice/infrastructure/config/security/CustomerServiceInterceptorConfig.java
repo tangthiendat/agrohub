95e015b465525880b6ea23ca5859a7302ee59e0e
Tang Thien Dat <thiendattang@gmail.com>
2025-03-31 14:28:50 +0700

    feat: add base config

---
+++ customer-service/src/main/java/com/ttdat/customerservice/infrastructure/config/security/CustomerServiceInterceptorConfig.java
@@ -0,0 +1,17 @@
+package com.ttdat.customerservice.infrastructure.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+@RequiredArgsConstructor
+public class CustomerServiceInterceptorConfig implements WebMvcConfigurer {
+    private final PermissionInterceptor permissionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(permissionInterceptor);
+    }
+}
